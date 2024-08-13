4e67ff09e5c337378b56e23fbc2c4a634fee287b
Raymond Phan <raymondphan00@gmail.com>
2024-08-13 15:53:40 -0400

    Created TodoList Application

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function TodoCard(props) {
+	const {children, handleDeleteTodo, handleEditTodo, index } = props
+	
+    return (
+        <li className='todoItem'>
+			{children}
+            <div className='actionsContainer'>
+				<button onClick={() => {
+					handleEditTodo(index)
+				}}>
+            		<i class="fa-regular fa-pen-to-square"></i>
+				</button>
+				<button onClick={() => {
+					handleDeleteTodo(index)
+				}}>
+					<i class="fa-solid fa-trash-can"></i>
+				</button>
+            </div>
+    </li>
+    )
+}
